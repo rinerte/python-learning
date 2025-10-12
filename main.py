24480c2506daac03f8fe26dc722354858913829d
rinerte <rinerte@gmail.com>
2025-10-12 15:24:23 +0900

    while loop

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # loops
+
+# while 1==1:
+#     print("Help! I`m stuck in the loop!")
+
+# name = ""
+
+# while len(name) == 0:
+#     name = input("Enter your name: ")
+# print("Hello "+name)
+
+name = None
+
+while not name:
+    name = input("Enter your name: ")
+print("Hello "+name)
