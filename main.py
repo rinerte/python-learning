b274bc96765e1717b014630cb4ba982d2b72091c
rinerte <rinerte@gmail.com>
2025-10-12 14:39:04 +0900

    end

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# math_functions
+import math
+
+pi = 3.14
+
+x = 1
+y = 2
+z = 3
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(-pi))
+# print(pow(pi,pi))
+# print(pow(pi,pow(pi,pi)))
+# print(math.sqrt(4))
+
+
+print(max(x,y,z))
+print(min(x,y,z))
