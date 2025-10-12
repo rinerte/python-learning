17984b3efbb91a161f96d039bd80786c8dfe0d29
rinerte <rinerte@gmail.com>
2025-10-12 15:19:22 +0900

    lesson 10

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # logical_operators
+
+temp = int(input("What is the temperature outside: "))
+
+if not(temp < 0 or temp > 30):
+    print("the weather is OK")
+    print("go outside!")
+elif not(temp>=0 and temp <= 30):
+    print("the weather is bad")
+    print("stay home!")
