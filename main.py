ac92b8d86545a422da481850e34e95bd21d68c30
rinerte <rinerte@gmail.com>
2025-10-12 10:57:57 +0900

    eol

---
+++ main.py
@@ -0,0 +1,14 @@
+name = "pasha"
+
+# # print(len(name))
+# print(name.find("P")) # 0
+# print(name.capitalize()) #Pasha
+# print(name.upper()) # PASHA
+# print(name.lower()) # pasha
+# print(name.isdigit()) 
+# print(name.isalpha()) # True
+# print(name.count("a")) # 2
+# print(name.replace("a","AHAHA")) 
+
+# print(name*100)
+
