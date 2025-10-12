cb75d2247d6cc916a21bf2edc82b646c54e26577
rinerte <rinerte@gmail.com>
2025-10-12 09:59:32 +0900

    end of variables video

--- main.py
+++ main.py
@@ -10,8 +10,21 @@
 
 #This is comment
 
-first_name = "Paevil"
-last_name = "Daedoor"
-age = 99
+# first_name = "Paevil"
+# last_name = "Daedoor"
+# age = 99
+# print("I am "+first_name+" "+last_name+" I am "+str(age))
+# height = 250.6 #this is float now, ok , haha
+# print(height)
 
-print("I am "+first_name+" "+last_name+" I am "+str(age))
+
+#Multiple assignment
+first_name,last_name,age = "Pavel", "D", 30
+
+print(first_name)
+print(last_name)
+print(age)
+
+#You can also do this
+he = hehe = hehehe = "hehehehehe"
+print(he+hehe+hehehe)
