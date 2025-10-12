05973a22423dbc1f317dc90112ee9d19438d78e2
rinerte <rinerte@gmail.com>
2025-10-12 14:55:55 +0900

    start and end

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# string_slicing
+#    string_slicing
+#    indexing[] or slice()
+#    [start:stop:step]
+
+name = "Hahah Heheh"
+first_name = name[:name.find(" ")]
+last_name = name[name.find(" ")+1:]
+
+funky_name = name[::2]
+reversed_name = name[::-1]
+
+# print(first_name)
+# print(last_name)
+# print(funky_name)
+# print(reversed_name)
+
+
+website = "http://google.com"
+website2 = "http://gagrugraraasfasf.com"
+
+slice = slice(7,-4)
+
+print(website[slice])
+print(website2[slice])
