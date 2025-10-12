9c285cf54550e891c991f8743d3eed145fcea207
rinerte <rinerte@gmail.com>
2025-10-12 09:43:32 +0900

    print different variables

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-hello = "Hi"
-print(hello)
+# variable="Hello"
+# print(variable)
+# print(type(variable))
+# variable = 555
+# print(variable)
+# print(type(variable))
+# variable = False
+# print(variable)
+# print(type(variable))
+
+#This is comment
+
+first_name = "Paevil"
+last_name = "Daedoor"
+age = 99
+
+print("I am "+first_name+" "+last_name+" I am "+str(age))
