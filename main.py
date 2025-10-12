3e3aae6ee94f056e768d94e7aa0d27eedbf17b84
rinerte <rinerte@gmail.com>
2025-10-12 15:07:08 +0900

    lesson 9

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # if_statements
+
+age = int(input("How old are you: "))
+
+if age < 18 and age>=0:
+    print("You are allowed")
+elif age < 0:
+    print("Hows that?!")
+else:
+    print("You are too old!")
