274599137da449ef6b13ac03edb3671820422a72
rinerte <rinerte@gmail.com>
2025-10-14 07:03:05 +0900

    ednd

--- main.py
+++ main.py
@@ -20,7 +20,38 @@ import os
 # except FileNotFoundError:
 #     print("File not found.")
 
-text = "This is a test file.\nThis is the second line."
+# text = "This is a test file.\nThis is the second line."
 
-with open("test2.txt", "w") as file:
-    file.write(text)
+# with open("test2.txt", "w") as file:
+#     file.write(text)
+
+
+# copyfile() = copies contents of a file
+# copy() = copies file and permission mode + destination can be a directory
+# copy2() = copies file, permission mode, and metadata (like creation and modification times)
+
+# import shutil
+
+# shutil.copyfile("test2.txt", "test3.txt")
+# shutil.copy("test2.txt", "C:\\Users\\paand\\Documents")
+# shutil.copy2("test2.txt", "test4.txt")
+
+
+# MOVING FILES
+
+import os
+
+source = "test.txt"
+destination = "C:\\Users\\paand\\Desktop\\test.txt"
+
+try:
+    if os.path.exists(destination):
+        print("File already exists at destination.")
+    else:
+        os.replace(source, destination)
+        print(f"Moved file from {source} to {destination}.")
+
+except FileNotFoundError as e:
+    print(e)
+    print("Source file not found.")
+
