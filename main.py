361e4977e7e0b5d73b8bc7c42bb5befbcd6f8d6d
rinerte <rinerte@gmail.com>
2025-10-13 18:39:24 +0900

    check file existance

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # files
+import os
+
+# path = "C:\\Users\\paand\\Documents\\example.txt"
+path = "C:\\Users\\paand\\Documents"
+
+if os.path.exists(path):
+    print("Location exists.")
+    if os.path.isfile(path):
+        print("It's a file.")
+    elif os.path.isdir(path):
+        print("It's a directory.")
+else:
+    print("File does not exist.")
