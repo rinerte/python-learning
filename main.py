5712982448588a24aee8b3b5005b28a3d52fe9b1
rinerte <rinerte@gmail.com>
2025-10-13 10:35:15 +0900

    lesson 16

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 # lists
+
+# food = ["pizza","fish","norma"]
+
+# print(food)
+# print(food[0]+food[1])
+
+# food[0] = "sushi"
+
+# print(food[0]+food[1])
+
+# food.append("cherry")
+# food.remove("fish")
+# food.pop()
+# food.insert(0,"gg")
+# food.sort()
+
+# for i in food:
+#     print(i)
+
+# food.clear()
+
+# 2D LISTSS
+
+drinks = ["coffee","soda","tee","water"]
+dinner = ["pizza","shit"]
+dessert = ["oil","butter","gas"]
+
+food = [drinks,dinner,dessert]
+
+print(food)
+
+for x in food:
+    for y in x:
+        print(y)
