98087e00c3e0a0dbdea8af60537389c0ef905935
rinerte <rinerte@gmail.com>
2025-10-13 10:57:25 +0900

    end of lesson

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# sets
+# sets = collection which is unordered, unindexed. no duplicate values
+
+utensils = {"fork","spoon","plate","plate","plate","plate"}
+dishes = {"bowl","plate","cup"}
+
+# utensils.add("napkin")
+# utensils.remove("plate")
+
+# for x in utensils:
+#     print(x)
+
+# utensils.clear()
+
+# utensils.update(dishes)
+
+# dinner_table = utensils.union(dishes)
+
+# for x in dinner_table:
+#     print(x)
+
+print(utensils.difference(dishes))
+print(utensils.intersection(dishes))
