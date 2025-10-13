16e0c0123cda07e63f742d98d2d2a1ee61f0614c
rinerte <rinerte@gmail.com>
2025-10-13 18:46:03 +0900

    read a file

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 # files
 import os
 
-# path = "C:\\Users\\paand\\Documents\\example.txt"
-path = "C:\\Users\\paand\\Documents"
+# # path = "C:\\Users\\paand\\Documents\\example.txt"
+# path = "C:\\Users\\paand\\Documents"
 
-if os.path.exists(path):
-    print("Location exists.")
-    if os.path.isfile(path):
-        print("It's a file.")
-    elif os.path.isdir(path):
-        print("It's a directory.")
-else:
-    print("File does not exist.")
+# if os.path.exists(path):
+#     print("Location exists.")
+#     if os.path.isfile(path):
+#         print("It's a file.")
+#     elif os.path.isdir(path):
+#         print("It's a directory.")
+# else:
+#     print("File does not exist.")
+
+try:
+    with open("tes.txt") as file:
+        print(file.read())
+except FileNotFoundError:
+    print("File not found.")
