5fb8cf2459c3566fe266544f2f45777c2aced650
rinerte <rinerte@gmail.com>
2025-10-14 10:56:50 +0900

    class

---
+++ car.py
@@ -0,0 +1,15 @@
+# oop
+
+class Car:
+
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def drive(self):
+        print("The car is driving")
+    
+    def stop(self):
+        print("The car has stopped")
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# oop
+from car import Car
+
+
+
+car_1 = Car("Toyota", "Camry", 2020, "Blue")
+car_2 = Car("Honda", "Civic", 2019, "Red")
+car_3 = Car("Ford", "Mustang", 2021, "Black")
+
+print(car_1.make)
+print(car_1.model)
+print(car_1.year)
+print(car_1.color)
+
+car_1.drive()
+car_1.stop()
+
+# Print list of method and attributes available in Car class
+
+print(dir(Car))
