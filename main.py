62788355029677f15d21fe6e84c0d73f814f1793
rinerte <rinerte@gmail.com>
2025-10-15 15:29:31 +0900

    abstract classes

--- main.py
+++ main.py
@@ -33,30 +33,57 @@
 
 # multiple inheritance
 
-class Prey:
-    def flee(self):
-        print("This animal is fleeing")
-class Predator:
-    def hunt(self):
-        print("This animal is hunting")
+# class Prey:
+#     def flee(self):
+#         print("This animal is fleeing")
+# class Predator:
+#     def hunt(self):
+#         print("This animal is hunting")
 
-class Rabbit(Prey):
-    def run(self):
-        print("This rabbit is running")
+# class Rabbit(Prey):
+#     def run(self):
+#         print("This rabbit is running")
 
-class Hawk(Predator):
-    def fly(self):
-        print("This hawk is flying")
+# class Hawk(Predator):
+#     def fly(self):
+#         print("This hawk is flying")
 
-class Fish(Prey, Predator):
-    def swim(self):
-        print("This fish is swimming")
+# class Fish(Prey, Predator):
+#     def swim(self):
+#         print("This fish is swimming")
 
-rabbit = Rabbit()
-hawk = Hawk()
-fish = Fish()
+# rabbit = Rabbit()
+# hawk = Hawk()
+# fish = Fish()
 
-rabbit.flee()
-hawk.hunt()
-fish.flee()
-fish.hunt()
+# Abstract classes
+# Prevents a user from creating an object of that class
+# + compels a user to override abstract methods in a child class
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def go(self):
+        pass
+    @abstractmethod
+    def stop(self):
+        pass
+
+class Car(Vehicle):
+    def go(self):
+        print("The car is driving")
+    def stop(self):
+        print("The car has stopped")
+
+class Motorcycle(Vehicle):
+    def go(self):
+        print("The motorcycle is driving")
+    def stop(self):
+        print("The motorcycle has stopped")
+
+# vehicle = Vehicle() # will raise an error
+car = Car()
+motorcycle = Motorcycle()
+car.go()
+motorcycle.go()
