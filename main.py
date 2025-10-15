4be8fd18f9bcf127f6d546ab4c21d415fb1b3507
rinerte <rinerte@gmail.com>
2025-10-16 06:59:03 +0900

    zip

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# zip
+# zip(*iterables) = aggregate lements from two or more iterables (lists, tuples, etc.)
+# creates a zip object with paired elements stored in tuples for each element
+
+usernames = ['samuel', 'carla', 'jeffrey']
+passwords = ['123', '456', '789']
+
+users = dict(zip(usernames, passwords))
+print(type(users))
+
+for key, value in users.items():
+    print(f'{key}: {value}')
