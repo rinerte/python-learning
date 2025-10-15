b2a011769275bde1d133e17c82ab05c43d637e1f
rinerte <rinerte@gmail.com>
2025-10-15 16:48:34 +0900

    sort

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # sort
+
+students = [("john", "F", 60),
+            ("jane", "B", 70),
+            ("dave", "F", 50),
+            ("paul", "A", 100),
+            ("kate", "A", 90),
+            ("lisa", "B", 80)]
+students.sort(key=lambda student: student[1]) # sort by grade
+
+for i in students:
+    print(i)
