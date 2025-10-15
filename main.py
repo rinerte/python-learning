e93e918e54819799fb28234e900dfd4f4b40b8d8
rinerte <rinerte@gmail.com>
2025-10-15 15:11:14 +0900

    multiple inheritance

--- main.py
+++ main.py
@@ -1,32 +1,62 @@
 # inheritance
-class Animal:
+# class Animal:
 
-    alive = True
-    def eat(self):
-        print("This animal is eating")
-    def sleep(self):
-        print("This animal is sleeping")
+#     alive = True
+#     def eat(self):
+#         print("This animal is eating")
+#     def sleep(self):
+#         print("This animal is sleeping")
     
-class Rabbit(Animal):
+# class Rabbit(Animal):
+#     def run(self):
+#         print("This rabbit is running")
+
+# class Fish(Animal):
+#     def swim(self):
+#         print("This fish is swimming")
+
+# class Hawk(Animal):
+#    def fly(self):
+#         print("This hawk is flying")
+
+# rabbit = Rabbit()
+# fish = Fish()
+# hawk = Hawk()
+
+# print(rabbit.alive)
+# fish.eat()
+# hawk.sleep()
+
+# rabbit.run()
+# fish.swim()
+# hawk.fly()
+
+# multiple inheritance
+
+class Prey:
+    def flee(self):
+        print("This animal is fleeing")
+class Predator:
+    def hunt(self):
+        print("This animal is hunting")
+
+class Rabbit(Prey):
     def run(self):
         print("This rabbit is running")
 
-class Fish(Animal):
+class Hawk(Predator):
+    def fly(self):
+        print("This hawk is flying")
+
+class Fish(Prey, Predator):
     def swim(self):
         print("This fish is swimming")
 
-class Hawk(Animal):
-   def fly(self):
-        print("This hawk is flying")
-
 rabbit = Rabbit()
-fish = Fish()
 hawk = Hawk()
+fish = Fish()
 
-print(rabbit.alive)
-fish.eat()
-hawk.sleep()
-
-rabbit.run()
-fish.swim()
-hawk.fly()
+rabbit.flee()
+hawk.hunt()
+fish.flee()
+fish.hunt()
