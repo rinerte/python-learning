58b827a509fa2af29421c48703c0045b810a64dc
rinerte <rinerte@gmail.com>
2025-10-15 16:58:52 +0900

    filter

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ reversed_students = sorted(students, key=lambda student: student[1], reverse=Tru
 # for i in students:
 #     print(i)
 
-# MAP 
+# MAP - applies a function to all items in an iterable
+# 
 
 store = [("shirt", 20.00),
          ("pants", 25.00),
@@ -22,5 +23,22 @@ store = [("shirt", 20.00),
 to_euros = lambda data: (data[0], data[1] * 0.82)
 store_euros = list(map(to_euros, store))
 
-for i in store_euros:
+# for i in store_euros:
+#     print(i)
+
+
+# Filter - creates a collection of elements from an iterable for which a function returns true
+#filter(function, iterable)
+
+friends = [("Rachel", 19),
+           ("Monica", 18),
+           ("Phoebe", 17),
+           ("Joey", 16),
+           ("Chandler", 21),
+           ("Ross", 20)]
+
+old_enough = lambda data: data[1] >= 18
+drinking_buddies = list(filter(old_enough, friends))
+
+for i in drinking_buddies:
     print(i)
