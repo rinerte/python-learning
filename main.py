e0e562f421ea6000299de9fd1ad3d800e6e413cf
rinerte <rinerte@gmail.com>
2025-10-16 10:02:24 +0900

    threads

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
 # threading
+
+# thread = a flow of execution. like a separate order of instructions.
+#          however each thread takes a turn running to achieve concurrency.
+#          GIL = (global interpreter lock), a lock that allows only one thread to hold
+#          the control of the Python interpreter. (to prevent data corruption)
+
+# cpu bound = program/task spends most of its time waiting for internal events (CPU intensive)
+# use multiprocessing
+
+# io bound = program/task spends most of its time waiting for external events (user input, web scraping, database)
+# use multithreading
+
+import threading
+import time
+
+# print(threading.active_count())
+# print(threading.enumerate())
+
+start = time.perf_counter()
+
+def eat_breakfast():
+    time.sleep(3)
+    print("You finished eating breakfast")
+
+def drink_coffee():
+    time.sleep(4)
+    print("You finished drinking coffee")
+
+def study():
+    time.sleep(5)
+    print("You finished studying")
+
+# eat_breakfast()
+# drink_coffee()
+# study()
+
+x = threading.Thread(target=eat_breakfast, args=())
+x.start()
+y = threading.Thread(target=drink_coffee, args=())
+y.start()
+z = threading.Thread(target=study, args=())
+z.start()
+print(threading.active_count())
+print(threading.enumerate())
+
+x.join()
+y.join()
+z.join()
+# performance counter in seconds
+
+print(f"Total time: {time.perf_counter()-start} seconds")
