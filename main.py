823e87b0c48b56e2d31f64195058ef81942af3a4
rinerte <rinerte@gmail.com>
2025-10-16 10:30:05 +0900

    multiprocessing

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-# multiprocessing
+# multiprocessing =
+
+from multiprocessing import Process, cpu_count
+import time
+
+def counter(num):
+    count = 0
+    while count < num:
+        count+=1
+
+def main():
+    start = time.perf_counter()
+
+    # a = Process(target=counter, args=(1_000_000_000,))
+    # a.start()
+    # b = Process(target=counter, args=(1_000_000_000,))
+    # b.start()
+    # a = Process(target=counter, args=(5_00_000_000,))
+    # a.start()
+    # b = Process(target=counter, args=(5_00_000_000,))
+    # b.start()
+    a = Process(target=counter, args=(250_000_000,))
+    a.start()
+    b = Process(target=counter, args=(250_000_000,))
+    b.start()
+    c = Process(target=counter, args=(250_000_000,))
+    c.start()
+    d = Process(target=counter, args=(250_000_000,))
+    d.start()
+
+    a.join()
+    b.join()
+    c.join()
+    d.join()
+
+    print("finished in: ",time.perf_counter()-start)
+    print(cpu_count())
+
+
+if __name__ == '__main__':
+    main()
+
+
