43c450fc688c470ccd27a773fd17d5a101826d64
rinerte <rinerte@gmail.com>
2025-10-16 10:40:26 +0900

    first window

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # GUI
+from tkinter import *
+
+
+window = Tk()
+window.geometry("420x1000")
+window.title("My cool custom title")
+
+icon = PhotoImage(file='baldHogLogo.png')
+window.iconphoto(True,icon)
+window.config(background="#c49029")
+window.mainloop() #place window on computer screen, listen for events
