fdbd68638b2c06cffc519422aea737b488eb1e4e
rinerte <rinerte@gmail.com>
2025-10-19 15:43:13 +0900

    end

--- main.py
+++ main.py
@@ -35,7 +35,50 @@ window.config(background=main_backcolor)
 #                 command=click)
 # button.pack()
 
+# ENTRYBOX
+
+# def submit():
+#     username = entry.get()
+#     print(f'Hello {username}')
+
+# def delete():
+#     entry.delete(0,END)
+
+# def bckspace():
+#     entry.delete(len(entry.get())-1,END)
+
+# entry = Entry(window,
+#               font=("Arial",20))
+# entry.pack(side=LEFT)
+
+# submit_button = Button(window, text="submit", command=submit)
+# submit_button.pack(side=RIGHT)
+
+# delete_button = Button(window, text="delete", command=delete)
+# delete_button.pack(side=RIGHT)
+
+# bck_button = Button(window, text="bckspace", command=bckspace)
+# bck_button.pack(side=RIGHT)
+
+
+# CHECKBOX
+
+x = IntVar()
+
+def Display():
+    if(x.get() == 1):
+        print("You agree")
+    else:
+        print("You don")
 
 
 
+check_btn = Checkbutton(window,
+                        text = "I agree",
+                        variable=x,
+                        onvalue=1,
+                        offvalue=0,
+                        command=Display)
+
+check_btn.pack()
 window.mainloop() #place window on computer screen, listen for events
