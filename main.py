4d50a27eff24dd8b18fe2d2f01a485ec1582737e
rinerte <rinerte@gmail.com>
2025-10-19 16:01:35 +0900

    1000 colors

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 # canvas
+from tkinter import *
+import time
+
+window = Tk()
+window.geometry("500x500")
+
+redness = 244
+greeness = 244
+blueness = 244
+
+position = 0
+
+for i in range(10):
+    for j in range(10):
+        for k in range(10):
+            color = f"#{redness:X}{greeness:X}{blueness:X}"
+            label = Label(window, width=1, height=1, bg=color)
+            label.place(x=0+position,y=0)
+            blueness+=1
+            position+=10
+        greeness+=1
+        blueness = 244
+    redness+=1
+    greeness = 244
+    
+
+
+window.mainloop()
